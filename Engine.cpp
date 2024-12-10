7f8fd3965a0022f82112eb0777a24087daa6d0b5
DrippinJones <129703208+DrippinJones@users.noreply.github.com>
2024-12-10 15:56:18 -0800

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -13,7 +13,7 @@ using namespace sf;
 Engine::Engine()
 {
 	VideoMode screen(VideoMode::getDesktopMode().width / 2, VideoMode::getDesktopMode().height / 2, Style::Default);
-	m_Window.create(screen, "temp");
+	m_Window.create(screen, "nootice");
 }
 
 // Run will call all the private functions
@@ -26,7 +26,14 @@ void Engine::run()
 	Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
 	p.unitTests();
 	cout << "Unit tests complete.  Starting engine..." << endl;
+
+	sf::SoundBuffer buffer;
+	if (!buffer.loadFromFile("hawkTuah.wav"))
+		return;
+
 	
+	sound.setBuffer(buffer);
+
 	while (m_Window.isOpen())
 	{
 		t1 = clock.restart();
@@ -44,6 +51,10 @@ void Engine::input()
 	Event event;
 	srand(time(0));
 
+	
+	
+	
+	
 	while (m_Window.pollEvent(event))
 	{
 		
@@ -61,15 +72,20 @@ void Engine::input()
 		{
 			if (event.mouseButton.button == Mouse::Left)
 			{
+				sound.play();
 
 				for (int i = 0; i < 5; i++)
 				{
 					int randNum = rand() % 26 + 25; // Random number [25, 50]
 					m_particles.push_back(Particle(m_Window, randNum, Vector2i(event.mouseButton.x, event.mouseButton.y)));
 				}
+
+				//sound.stop();
 			}
+
 		}
 	}
+	
 }
 
 void Engine::update(float dtAsSeconds)
@@ -94,6 +110,21 @@ void Engine::draw()
 {
 	m_Window.clear();
 
+	sf::Texture backgroundTexture;
+	if (!backgroundTexture.loadFromFile("brainrot.png"))
+		return;
+
+	Vector2f spriteSize(900.0f, 1200.0f);
+
+	Sprite backgroundSprite(backgroundTexture);
+	backgroundSprite.setScale(spriteSize.x / backgroundSprite.getLocalBounds().width, spriteSize.x / backgroundSprite.getLocalBounds().height);
+
+	//m_Window.clear();
+	//backgroundSprite.setScale(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height);
+	m_Window.draw(backgroundSprite);
+	//m_Window.display();
+
+	
 	for (int i = 0; i < m_particles.size(); i++)
 	{
 		m_Window.draw(m_particles.at(i));
